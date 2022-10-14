c71dc185d6747d15ea333e4b644685d6c7ff2886
Azam Din <adin786@gmail.com>
2022-10-14 00:59:44 +0000

    added weather download

--- scripts/01-download_raw.py
+++ scripts/01-download_raw.py
@@ -9,16 +9,18 @@ REPO_ROOT = Path(repo_root())
 def main():
     print('Starting file download script')
 
-    URL_ENERGY = 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1107641/ET_1.2_SEP_22.xlsx'
+    URL_ENERGY = 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1012959/Total_Energy_ODS.ods'
+    URL_WEATHER = 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1012964/Weather_ODS.ods'
     DEST_PATH = REPO_ROOT / 'data' / 'raw'
-    print(f"Downloading file from {URL_ENERGY}")
 
-    dest_file = download_file(URL_ENERGY, DEST_PATH)
+    for url in [URL_ENERGY, URL_WEATHER]:
+        print(f"Downloading file from {url}")
 
-    if dest_file.is_file():
-        print(f'File downloaded to {dest_file}')
-    else:
-        print(f'Download failed')
+        dest_file = download_file(url, DEST_PATH)
+        if dest_file.is_file():
+            print(f'File downloaded to {dest_file}')
+        else:
+            print(f'Download failed')
 
     print('Finished file download script')
 
