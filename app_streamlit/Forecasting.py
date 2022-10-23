38641d59a459c16aade1f9f6756a7d5ad7fbc909
adin786@gmail.com <adin786@gmail.com>
2022-10-24 00:50:52 +0100

    Added template for lambda func + streamlit

---
+++ app_streamlit/Forecasting.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+
+st.title("Energy consumption forecasting")
+
+"""
+This app computes timeseries forecasts for
+UK monthly energy consumption.
+
+Use the sidebar to switch between this 
+interactive forecaster tool, a visual analysis of 
+energy trends, and discussion of forecasting model 
+results.
+
+"""
+
+
