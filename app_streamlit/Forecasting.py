14e3bc29dbac6f91186d5b1072b661282969b7e8
adin786@gmail.com <adin786@gmail.com>
2022-10-24 01:03:10 +0100

    streamlit app details

--- app_streamlit/Forecasting.py
+++ app_streamlit/Forecasting.py
@@ -1,8 +1,19 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import energy_forecast as ef
+from pathlib import Path
 
+REPO_ROOT = Path(ef.repo_root())
+DATA_DIR = REPO_ROOT / "data"
 
+@st.cache
+def load_df(path):
+    return ef.loaders.load_and_fix_types(path)
+    
+df = load_df(DATA_DIR / "processed" / "df_train.csv")
 
-st.title("Energy consumption forecasting")
+st.title("Energy consumption forecasting (UK)")
 
 """
 This app computes timeseries forecasts for
@@ -12,7 +23,10 @@ Use the sidebar to switch between this
 interactive forecaster tool, a visual analysis of 
 energy trends, and discussion of forecasting model 
 results.
-
 """
 
+# Plot 
 
+selected_date = st.date_input("Select a target date for the forecast")
+
+btn_state = st.buttom()
