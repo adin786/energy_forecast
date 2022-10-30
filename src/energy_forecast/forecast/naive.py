d520675e15c7afc4377de849d1e714fc2f491e8a
adin786@gmail.com <adin786@gmail.com>
2022-10-30 23:51:54 +0000

    model class update

--- src/energy_forecast/forecast/naive.py
+++ src/energy_forecast/forecast/naive.py
@@ -13,22 +13,49 @@ class Model:
     def __init__(self, estimator, *args, **kwargs) -> None:
         self.model = estimator(*args, **kwargs)
         self.params = self.model.get_params(deep=True)
+        self.run_name = ''
+        logger.debug('INIT: model intialised')
         
-    def fit(self, y: Optional[pd.DataFrame], x: Optional[pd.DataFrame]) -> None:
+    def fit(self, 
+            y_train: Optional[pd.DataFrame],
+            x_train: Optional[pd.DataFrame],
+            y_test: Optional[pd.DataFrame], 
+            x_test: Optional[pd.DataFrame], 
+           ) -> None:
         # May need to handle optional x
         # Some models dont use exogenous variables
+        logger.debug('FIT: running .fit() method')
+        exogenous = False if (x_train is None) else True
+        if exogenous:
+            logger.debug(f'FIT: {y_train.shape=}, {x_train.shape}')
+        else:
+            logger.debug(f'FIT: {y_train.shape=}')
         
         # May need to handle setting experiment etc
         with mlflow.start_run(), open('stdout.log', 'w') as f1, open('stderr.log', 'w') as f2:
+            self.run_name = mlflow.get_run_name()
             with redirect_stdout(f1), redirect_stderr(f2):
                 logger.debug('FIT: starting mlflow tracking')
                 mlflow.log_params(self.params)
-                logger.debug(f'FIT: model params tracked ({len(self.params)})')
+                logger.debug(f'FIT: model params tracked ({len(self.params)} values)')
                 
+                logger.debug('FIT: training model')
                 t0 = perf_counter()
-                self.model.fit(y, x)
+                if exogenous:
+                    self.model.fit(y_train)
+                else:
+                    self.model.fit(y_train, x_train)
                 t1 = perf_counter()
                 mlflow.log_metric('train_time', t1 - t0)
+                logger.debug(f'FIT: finished training model, took {t1-t0:.2f} seconds')
+                
+                # Evaluate
+                fh = ForecastHorizon(y_test.index)
+                if exogenous:
+                    y_pred = self.model.predict(fh, x_test)
+                else:
+                    y_pred = self.model.predict(fh)
+                
                 
             
             
